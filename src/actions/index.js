747a735532432a56c8442ab7e80847830bb3202f
Alex Beals <alexcbeals@gmail.com>
2016-08-03 20:54:31 -0400

    working on actions

---
+++ src/actions/index.js
@@ -0,0 +1,78 @@
+import axios from 'axios';
+
+// keys for actiontypes
+export const ActionTypes = {
+  FETCH_POSTS: 'FETCH_POSTS',
+  FETCH_POST: 'FETCH_POST',
+  CREATE_POST: 'CREATE_POST',
+  UPDATE_POST: 'UPDATE_POST',
+  DELETE_POST: 'DELETE_POST',
+};
+
+const ROOT_URL = 'https://cs52-blog.herokuapp.com/api';
+const API_KEY = '?key=A_BEALS';
+
+export function fetchPosts() {
+  return (dispatch) => {
+    axios.get(`${ROOT_URL}/posts${API_KEY}`).then(response => {
+      dispatch({
+        type: 'FETCH_POSTS',
+        payload: response.data,
+      });
+    }).catch(error => {
+      console.log(error);
+    });
+  };
+}
+
+export function fetchPost(id) {
+  return (dispatch) => {
+    axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`).then(response => {
+      dispatch({
+        type: 'FETCH_POST',
+        payload: response.data,
+      });
+    }).catch(error => {
+      console.log(error);
+    });
+  };
+}
+
+export function createPost(post) {
+  return (dispatch) => {
+    axios.post(`${ROOT_URL}/posts/${API_KEY}`, post).then(response => {
+      dispatch({
+        type: 'CREATE_POST',
+        payload: response.data,
+      });
+    }).catch(error => {
+      console.log(error);
+    });
+  };
+}
+
+export function updatePost(id, post) {
+  return (dispatch) => {
+    axios.put(`${ROOT_URL}/posts/${id}${API_KEY}`, post).then(response => {
+      dispatch({
+        type: 'UPDATE_POST',
+        payload: response.data,
+      });
+    }).catch(error => {
+      console.log(error);
+    });
+  };
+}
+
+export function deletePost(id) {
+  return (dispatch) => {
+    axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`).then(response => {
+      dispatch({
+        type: 'DELETE_POST',
+        payload: response.data,
+      });
+    }).catch(error => {
+      console.log(error);
+    });
+  };
+}
---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const NavBar = (props) => {
+  return (
+    <div className="nav">
+      <Link to="/">Alex's Blog</Link>
+      <Link to="posts/new">new post</Link>
+    </div>
+    );
+};
+
+export default NavBar;
