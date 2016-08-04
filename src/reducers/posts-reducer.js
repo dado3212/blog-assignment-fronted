1d59f91b996af821cdfe4ea65e7a00ddca21cc7c
Alex Beals <alexcbeals@gmail.com>
2016-08-03 20:58:17 -0400

    set up reducers

---
+++ src/reducers/posts-reducer.js
@@ -0,0 +1,14 @@
+import { ActionTypes } from '../actions';
+
+const PostsReducer = (state = { all: [], currentPost: {} }, action) => {
+  switch (action.type) {
+    case ActionTypes.FETCH_POSTS:
+      return { all: action.payload, currentPost: state.currentPost };
+    case ActionTypes.FETCH_POST:
+      return { all: state.all, currentPost: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default PostsReducer;
