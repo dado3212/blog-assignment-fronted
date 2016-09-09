7f55b1ef97d8417251fced8049f56f668213f2da
Alex Beals <alexcbeals@gmail.com>
2016-09-09 11:26:07 -0700

    minor change:

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import PostsReducer from './posts-reducer';
 import AuthReducer from './auth-reducer';
+import GroupsReducer from './groups-reducer';
 
 const rootReducer = combineReducers({
   posts: PostsReducer,
   auth: AuthReducer,
+  groups: GroupsReducer,
 });
 
 export default rootReducer;
